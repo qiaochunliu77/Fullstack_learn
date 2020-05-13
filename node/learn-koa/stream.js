75d7e9f3312b9f1debdd9a5cef82a81f14b4cd93
qiaochunliu77 <liuqiaochun_99@qq.com>
2020-05-13 23:14:25 +0800

    5.13 koastatic

--- node/learn-koa/stream.js
+++ node/learn-koa/stream.js
@@ -1,32 +1,31 @@
 const fs = require('fs');
 const Koa = require('koa'); // web node 开发框架
 const app = new Koa();
-
+const KoaStatic = require('koa-static')
+// const main = ctx => {
+//     console.log(ctx.request.url + '----------------')
+//     if (ctx.request.url === '/1.jpg') {
+//         ctx.response.type = 'image'
+//         ctx.response.body = fs.createReadStream('./1.jpg')
+//     } else if (ctx.request.url === '/common.css') {
+//         ctx.response.type = 'css';
+//         ctx.response.body = fs.createReadStream('./common.css');
+//     } else if(ctx.request.url ==='/common.js'){
+//         ctx.response.type = 'js'
+//         ctx.response.body = fs.createReadStream('./common.js')
+//     }else{
+//         ctx.response.type = 'html'; // 响应头  png  css js
+//         //在服务器端 打开可读流， TCP 
+//         ctx.response.body = fs.createReadStream('./template.html');
+//     }
+// }
 const main = ctx => {
-    console.log(ctx.request.url + '----------------')
-    if (ctx.request.url === '/1.jpg') {
-        ctx.response.type = 'image'
-        ctx.response.body = fs.createReadStream('./1.jpg')
-    } else if (ctx.request.url === '/common.css') {
-        ctx.response.type = 'css';
-        ctx.response.body = fs.createReadStream('./common.css');
-    } else if(ctx.request.url ==='/common.js'){
-        ctx.response.type = 'js'
-        ctx.response.body = fs.createReadStream('/common.js')
-    }else{
         ctx.response.type = 'html'; // 响应头  png  css js
         //在服务器端 打开可读流， TCP 
         ctx.response.body = fs.createReadStream('./template.html');
-    }
 
-    // const main = ctx => {
-    //   if (ctx.request.path !== '/') {
-    //     ctx.response.type = 'html';
-    //     ctx.response.body = '<a href="/">Index Page</a>';
-    //   } else {
-    //     ctx.response.body = 'Hello World';
-    //   }
-    // }
 }
-    app.use(main);
+// 先引入static静态文件 
+    app.use(KoaStatic('./'));
+    app.use(main)
     app.listen(8080);
