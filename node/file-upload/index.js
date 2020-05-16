bc566b06d492ddcac304e978713f1f1f9914928c
qiaochunliu77 <liuqiaochun_99@qq.com>
2020-05-16 16:50:41 +0800

    5.15 文件上传

--- node/file-upload/index.js
+++ node/file-upload/index.js
@@ -1,42 +1,40 @@
 const http = require('http');
 const fs = require('fs');
-http.createServer((req,res) => {
-    if(req.url === '/files' && req.method.toLowerCase === 'post'){
-        // 处理文件
-        pathFile(req,res);
-    }else{
-        fs.createReadStream('./index.html').pipe(res);
-    }
-    
+http.createServer((req, res) => {
+  if (req.url === '/files' && req.method.toLowerCase() === 'post') {
+    // 处理文件
+    parseFile(req, res);
+  } else {
+    fs.createReadStream('./index.html').pipe(res);
+  }
 })
-.listen(9090,() => {
-    console.log(9090)
+.listen(9090, () => {
+  console.log(9090);
 })
-function pathFile (req,res) {
-    let body = ''
-    let boundary = req.headers['content-type'].split('boundary=')[1]
-    req.on('data',(chunk) => {
-        body += chunk;
-    })
-    req.on('end',() => {
-        let lists = body.split('--'+boundary);
-        for(let list of lists){
-            // 文件上传
-            let lines = list.split('\r\n')
-            if(lines[1].includes('filename=')){
-                let startIndex = lines[1].indexOf('filename=');
-                fileName = lines[1].substring(startIndex);
-                console.log('filename',fileName)
-                fs.writeFile(`./${fileName}`,lines[4],{encoding:'binary'},(err)=> {
-                    if(!err){
-                        res.end('ok')
-                    }else{
-                        res.end(err.toString())
-                    }
-                })
-            }
-            
+function parseFile(req, res) {
+  req.setEncoding('binary');
+  let body = ''
+  let boundary = req.headers['content-type'].split('boundary=')[1];
+  // console.log(boundary);
+    // 开始传输数据 拼接起来
+  req.on('data', (chunk) => {
+    body += chunk;
+  })
+//   当传输完
+  req.on('end', () => {
+    // 单个文件  jpeg图片
+    let lines = body.split('Content-Type: image/jpeg\r\n\r\n');
+    let end = lines[1].indexOf('--' + boundary + '--') - 2
+    let binary = lines[1].substring(0, end);
+    fs.writeFile('./upload.jpg', binary, {
+      encoding: 'binary', 
+    }, (err) => {
+        if (!err) {
+          res.end('ok');
+        } else {
+          res.end(err.toString())
         }
     })
-    // console.log(boundary)
+  })
+
 }
