7f011d141a065eb81c9ea567452a1bb9ad7ced50
qiaochunliu77 <liuqiaochun_99@qq.com>
2020-05-29 11:35:12 +0800

    5.29

--- source_code/koa_code/lib/application.js
+++ source_code/koa_code/lib/application.js
@@ -3,18 +3,17 @@ const http = require('http');
 class MyKoa {
     constructor() {
     //   手写代码 源码分析
+    this.fn = undefined;
     console.log('ok')
     }
     // lisiten use
     // 有没有什么 es6的写法 
     listen(...args) {
-        let server = http.createServer((req,res) => {
-            res.end('hello world');
-        })
+        let server = http.createServer(this.fn)
         server.listen(...args);
     }
-    use() {
-
+    use(fn) {
+        this.fn = fn;
     }
 }
 
