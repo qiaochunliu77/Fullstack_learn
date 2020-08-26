3570273e786d3a0f80e9a912b5a96d3a138564d0
qiaochunliu77 <1358238105@qq.com>
2020-08-26 17:07:52 +0800

    elementUI源码 button

--- js/react合成事件/element-codee/src/components/ElButton.vue
+++ js/react合成事件/element-codee/src/components/ElButton.vue
@@ -2,33 +2,70 @@
   <button
     @click="handleClick"
     class="el-button"
-    :class="[type?'el-button--'+type:'',
+    :disabled="loading"
+    :type="nativeType"
+    :autofocus="autofocus"
+    :class="[
+    type?'el-button--'+type:'',
+    buttonSize?'el-button--'+buttonSize:'',
     {
         'is-round': round,
-        'is-circle': circle
+        'is-circle': circle,
+        'is-loading': loading,
+        'is-disabled':buttonDisabled,
+        'is-plain': plain
     }
   ]"
   >
-    <slot></slot>
+    <i class="el-icon-loading" v-if="loading"></i>
+    <i :class="icon" v-if="icon"></i>
+    <!-- v-if="!icon"  如果有图标 slot失效 -->
+    <span v-if="$slots.defalt">
+      <slot></slot>
+    </span>
   </button>
 </template>
 <script>
 // props 检查 结构
 // vue怎么做
 export default {
+  name: 'ElButton',
   // state
   data() {
     return {
       tit: "hello"
     };
   },
+  computed: {
+    buttonDisabled() {
+      return this.disabled;
+    },
+    // 按钮的大小
+    buttonSize() {
+      return this.size;
+    }
+  },
   props: {
+    size: {
+      type: String,
+      defalt: ""
+    },
+    autofocus: Boolean,
+    nativeType: {
+      type: String,
+      defalt: "button"
+    },
     type: {
       type: String,
       defalt: "default"
     },
     round: Boolean,
-    circle: Boolean
+    circle: Boolean,
+    loading: Boolean,
+    plain: Boolean,
+    icon: {
+      type: String
+    }
   },
   //   生命周期函数
   created() {
@@ -37,7 +74,7 @@ export default {
   //  事件
   methods: {
     handleClick(evt) {
-        this.$emit('click',evt)
+      this.$emit("click", evt);
     }
   }
 };
---
+++ js/react合成事件/element-codee/src/components/form/form-item.vue
@@ -0,0 +1,21 @@
+<template>
+  <div
+    class="el-form-item"
+    :class="[{
+        'el-form-item--feedback': elForm&&elForm.statusIcon
+    }]"
+  ></div>
+</template>
+
+<script>
+export default {
+  name: "ElFormItem",
+  data() {
+    return {};
+  },
+  props: {},
+  computed: {},
+  created() {},
+  inject: ["elForm"]
+};
+</script>
